6d84e4c231c23609b1f67f9692507d06f0afed88
Amarjeet Chauhan <rahulcse1@gmail.com>
2019-05-18 10:35:36 +0530

    Added 2 unit test to test startGame and preparePlayer method.

--- src/test/java/com/marketpulse/core/pingpong/AppTest.java
+++ src/test/java/com/marketpulse/core/pingpong/AppTest.java
@@ -1,38 +1,58 @@
 package com.marketpulse.core.pingpong;
 
+import java.util.Map;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	Championship championship = new Championship();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	public void testStartGame() {
+
+		Integer gameid = 1;
+		Integer playerid1 = 1;
+		Integer playerid2 = 2;
+
+		Game game = new Game();
+
+		Player player1 = championship.preparePlayer(playerid1, gameid);
+		game.setPlayer1(player1);
+		Player player2 = championship.preparePlayer(playerid2, gameid);
+		player2.setOffensive(false);
+		game.setPlayer2(player2);
+
+		Map<Integer, Integer> gamescore = game.getGamescore();
+		gamescore.put(playerid1, 0);
+		gamescore.put(playerid2, 0);
+
+		boolean startGame = false;
+		// player need to win 3 games to win the championship.
+		for (int i = 0; i < 3; i++) {
+			startGame = championship.startGame(game, player1, player2, gamescore);
+		}
+		assertTrue(startGame);
+	}
+
+	public void testPreparePlayer() {
+
+		Integer gameid = 1;
+		Integer playerid1 = 1;
+
+		Player player = championship.preparePlayer(playerid1, gameid);
+
+		assertEquals(player.getPlayerName(), "Joey");
+		assertEquals((int) player.getDefensiveSize(), 7);
+		assertEquals(player.isOffensive(), true); // by default setting true.
+	}
 }
